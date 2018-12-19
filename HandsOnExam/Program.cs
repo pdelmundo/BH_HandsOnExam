88fcf27f662346319d891a5f77ca82946e11c3d6
Ivan Kester <ivan.marcalas@barhead.com>
2018-12-19 10:46:45 +0800

    Changed 'Hello World' to 'Ivan Kester: nvi707'

--- HandsOnExam/Program.cs
+++ HandsOnExam/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace HandsOnExam
 {
@@ -6,7 +6,7 @@ namespace HandsOnExam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ivan Kester: nvi707!");
         }
     }
 }
